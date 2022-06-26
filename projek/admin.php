79b0eed15c570b176fed4138f81ce4e95c4eb30e
nur alifia <softech.series@gmail.com>
2022-06-26 12:39:49 +0700

    first commit

---
+++ projek/admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA_compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Admin desa</title>
+	<link rel="stylesheet" type="text/css" href="admin.css">
+</head>
+<body>
+	<div class="container">
+		<div class="login">
+			<form action="">
+				<h1>Login</h1>
+				<hr>
+				<p>Login Admin Website Desa</p>
+				<label for>Email</label>
+				<input type="text" placeholder="example@gmail.com">
+				<label for="">Password</label>
+				<input type="password" placeholder="password">
+				<button>Login</button>
+				<p>
+					<a href="#">Lupa Password</a>
+				</p>
+			</form>
+		</div>
+		<div class="right">
+			<img src="foto/Bojonegoro.png">	
+		</div>
+	</div>
+</body>
+</html>
---
+++ projek/fpdf/fpdf_bandung.php
@@ -0,0 +1,75 @@
+<?php
+//koneksi ke database
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbnm = "projek";
+
+$conn = mysqli_connect($host, $user, $pass);
+if ($conn) {
+$open = mysqli_select_db($conn, $dbnm);
+if (!$open) {
+die ("Database tidak dapat dibuka karena " . mysqli_error($conn));
+}
+} else {
+die ("Server MySQL tidak terhubung karena " . mysqli_error($conn));
+}
+//akhir koneksi
+
+#ambil data di tabel dan masukkan ke array
+$query = "SELECT * FROM fpdf_bandung ORDER BY nama";
+$sql = mysqli_query ($conn, $query);
+$data = array();
+while ($row = mysqli_fetch_assoc($sql)) {
+array_push($data, $row);
+}
+
+#setting judul laporan dan header tabel
+$judul = "Data Penduduk Dusun Bandung Desa Bandungrejo";
+$header = array(
+array("label"=>"Id", "length"=>5, "align"=>"L"),
+array("label"=>"Nama", "length"=>15, "align"=>"L"),
+array("label"=>"NIK", "length"=>20, "align"=>"L"),
+array("label"=>"Jenis Kelamin", "length"=>23, "align"=>"L"),
+array("label"=>"Tempat Tanggal Lahir", "length"=>45, "align"=>"L"),
+array("label"=>"Agama", "length"=>15, "align"=>"L"),
+array("label"=>"Alamat", "length"=>70, "align"=>"L")
+);
+
+#sertakan library FPDF dan bentuk objek
+require_once ("fpdf.php");
+$pdf = new FPDF();
+$pdf->AddPage();
+
+#tampilkan judul laporan
+$pdf->SetFont('Arial','B','16');
+$pdf->Cell(0,20, $judul, '0', 1, 'C');
+
+#buat header tabel
+$pdf->SetFont('Arial','','10');
+$pdf->SetFillColor(255,0,0);
+$pdf->SetTextColor(255);
+$pdf->SetDrawColor(128,0,0);
+foreach ($header as $kolom) {
+$pdf->Cell($kolom['length'], 5, $kolom['label'], 1, '0', $kolom['align'], true);
+}
+$pdf->Ln();
+
+#tampilkan data tabelnya
+$pdf->SetFillColor(224,235,255);
+$pdf->SetTextColor(0);
+$pdf->SetFont('');
+$fill=false;
+foreach ($data as $baris) {
+$i = 0;
+foreach ($baris as $cell) {
+$pdf->Cell($header[$i]['length'], 5, $cell, 1, '0', $kolom['align'], $fill);
+$i++;
+}
+$fill = !$fill;
+$pdf->Ln();
+}
+
+#output file PDF
+$pdf->Output();
+?>
